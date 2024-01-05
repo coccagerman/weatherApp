ce77f89a88d9c21672fb3cabf80349fed0291a29
Germán Cocca <coccagerman@gmail.com>
2024-01-05 15:52:47 -0300

    apikey change

--- src/services/url.path.ts
+++ src/services/url.path.ts
@@ -4,8 +4,7 @@ export const urlPath = {
     getWeather: 'http://api.openweathermap.org/data/2.5/weather?'
 }
 
-// export const apiKey = 'eadea9e03f0b4664a6f737acfaa15da2'
-export const apiKey = '0e05f20cd4d10dff898cc65a6acd9013'
+export const apiKey = 'eadea9e03f0b4664a6f737acfaa15da2'
 
 export const headers = {
     headers: {
